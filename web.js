3be34f1704e91fe384e22d43a1edb76e76e92722
Wyatt Pearsall <wyattttttt@gmail.com>
2013-07-12 15:50:00 +0000

    fixed webjs

--- web.js
+++ web.js
@@ -1,9 +1,8 @@
-var express = require('express'),fs=require('fs'), app = express.createServer(express.logger());
-fs.readFile('./index.html',function(err,data){
+var express = require('express'),fs=require('fs'), app = express.createServer(express.logger()){
 
 app.get('/', function(request, response) {
-  response.send(data);
-});});
+  response.send(fs.readFileSync('./index.html'))
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
