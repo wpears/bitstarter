ef39ef5f4249064cc2175c7877a7ff2237da9e79
Wyatt Pearsall <wyattttttt@gmail.com>
2013-07-12 03:19:45 +0000

    readfromfile

--- web.js
+++ web.js
@@ -1,10 +1,9 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
+var express = require('express'),fs=require('fs'),r app = express.createServer(express.logger());
+fs.readFile('index.html',function(err,data){
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+  response.send(data);
+});});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
