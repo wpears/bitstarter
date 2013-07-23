d541b00b9c1251832419aaadf5a6d6e97e72da89
Wyatt Pearsall <wyattttttt@gmail.com>
2013-07-23 05:07:08 +0000

    bootstrap

--- grader.js
+++ grader.js
@@ -1,28 +1,10 @@
 #!/usr/bin/env node
-/*utomatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
-
-References:
-
- + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
-
- + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
-
- + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
-*/
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
+var REST_DEFAULT ='http://evening-thicket-4366.herokuapp.com';
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -53,7 +35,19 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     }
     return out;
 };
-
+var checkUrl=function(url,checksfile){
+var out={};
+restler.get(url).on('complete',function(page){
+	var cheeriofn=cheerio.load(page);
+	var loadchecks=loadChecks(checksfile).sort()
+	for (var j in loadchecks){
+		var curr = cheeriofn(loadchecks[j]).length>0;
+		out[loadchecks[j]]=present;
+	}
+});
+console.log(JSON.stringify(out,null,4));
+return out;
+}
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -64,10 +58,17 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>','Requested url',REST_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+	if(program.file){
+     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
-} else {
+	}else{
+	if(program.url)
+	var checkJson=checkUrl(program.url,program.checks)
+	}
+}else{
     exports.checkHtmlFile = checkHtmlFile;
+	exports.checkUrl=checkUrl;
 }
